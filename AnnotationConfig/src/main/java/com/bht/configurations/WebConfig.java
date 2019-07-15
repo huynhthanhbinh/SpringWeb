c4d85224bc04ce157865975f9644afcc76c21916
Richard Huynh <huynhthanhbinh1998@hotmail.com>
2019-07-16 01:58:07 +0700

    Spring Properties Files with @PropertySource : classpath/file

--- AnnotationConfig/src/main/java/com/bht/configurations/WebConfig.java
+++ AnnotationConfig/src/main/java/com/bht/configurations/WebConfig.java
@@ -1,10 +1,14 @@
 package com.bht.configurations;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.multipart.MultipartResolver;
@@ -22,7 +26,20 @@ import javax.sql.DataSource;
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.bht")
+@PropertySource("classpath:db.properties")
 public class WebConfig implements WebMvcConfigurer {
+
+
+    // In Spring, we can use annotation @PropertySource
+    // to externalize our configurations to
+    // a properties file under src/main/resources folder
+    // Therefore, we can get these values through
+    // @Value annotation or Environment object(@Autowired)
+    // For eg. @Value("${mssql.url}") private String url
+    // Remind: using # for comment in properties file !
+    @Autowired
+    Environment environment;
+
 
     // Configure viewResolver for matching view name return by Controller
     @Bean
@@ -80,10 +97,10 @@ public class WebConfig implements WebMvcConfigurer {
     public DataSource dataSource() {
 
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        dataSource.setUrl("jdbc:sqlserver://localhost:1433;database=BHT");
-        dataSource.setUsername("sa");
-        dataSource.setPassword("P@ssw0rd");
+        dataSource.setDriverClassName(environment.getProperty("mssql.DriverClassName"));
+        dataSource.setUrl(environment.getProperty("mssql.Url"));
+        dataSource.setUsername(environment.getProperty("mssql.Username"));
+        dataSource.setPassword(environment.getProperty("mssql.Password"));
 
         return dataSource;
     }
@@ -92,5 +109,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Bean
     public JdbcTemplate jdbcTemplate() {
         return new JdbcTemplate(dataSource());
+    }
+
+
+    // As Environment obj is in the same class
+    // Configurer method need to be STATIC one
+    // As environment object can access directly
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer
+    placeholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
     }
 }
